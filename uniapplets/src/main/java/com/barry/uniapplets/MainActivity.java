0a7f495d34d31dfed60a4e059401491ad5019eb2
Barry <865068275@qq.com>
2020-09-02 00:57:19 +0800

    集成uni小程序SDK

--- uniapplets/src/main/java/com/barry/uniapplets/MainActivity.java
+++ uniapplets/src/main/java/com/barry/uniapplets/MainActivity.java
@@ -1,8 +1,11 @@
 package com.barry.uniapplets;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import io.dcloud.feature.sdk.DCUniMPSDK;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_applets).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    DCUniMPSDK.getInstance().startApp(MainActivity.this,"__UNI__04E3A11");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
