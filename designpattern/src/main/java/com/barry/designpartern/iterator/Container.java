68d098f76e93065ff91a9434ea597b9185b1ecaa
Barry <865068275@qq.com>
2020-10-21 10:33:58 +0800

    迭代器模式

---
+++ designpattern/src/main/java/com/barry/designpartern/iterator/Container.java
@@ -0,0 +1,5 @@
+package com.barry.designpartern.iterator;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ designpattern/src/main/java/com/barry/designpartern/iterator/IteratorPatternDemo.java
@@ -0,0 +1,12 @@
+package com.barry.designpartern.iterator;
+
+public class IteratorPatternDemo {
+    public static void main(String[] args) {
+        NameRepository nameRepository = new NameRepository();
+
+        for (Iterator iter = nameRepository.getIterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            System.out.println("Name : " + name);
+        }
+    }
+}
