8c7307363cfbdb9c789e2bc41d31854e6f076d54
Barry <865068275@qq.com>
2020-10-23 14:21:04 +0800

    用Canvas画Path

--- customview/src/main/java/com/barry/customview/canvas/CanvasShowActivity.java
+++ customview/src/main/java/com/barry/customview/canvas/CanvasShowActivity.java
@@ -23,5 +23,6 @@ public class CanvasShowActivity extends AppCompatActivity {
         findViewById(R.id.btn_draw_circle).setOnClickListener(view -> startActivity(new Intent(this, DrawCircleActivity.class)));
         findViewById(R.id.btn_draw_oval).setOnClickListener(view -> startActivity(new Intent(this, DrawOvalActivity.class)));
         findViewById(R.id.btn_draw_arc).setOnClickListener(view -> startActivity(new Intent(this, DrawArcActivity.class)));
+        findViewById(R.id.btn_draw_path).setOnClickListener(view -> startActivity(new Intent(this, DrawPathActivity.class)));
     }
 }
