9e0f8932550f81b28dba294373e8c7300015a169
Barry <865068275@qq.com>
2020-10-25 18:04:40 +0800

    MotionLayout的简单使用

--- motionlayout/src/main/java/com/barry/motionlayout/SimpleMotionActivity.java
+++ motionlayout/src/main/java/com/barry/motionlayout/SimpleMotionActivity.java
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SimpleMotionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_simple_motion).setOnClickListener((view) -> startActivity(new Intent(this, SimpleMotionActivity.class)));
+        setContentView(R.layout.activity_simple_motion);
     }
 }
