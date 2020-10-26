40c74533d85ad5c644e77cc1824a8d0eed1dbade
Barry <865068275@qq.com>
2020-10-26 14:30:11 +0800

    创建animation工程

---
+++ animation/src/main/java/com/barry/animation/tween/TranslateActivity.kt
@@ -0,0 +1,12 @@
+package com.barry.animation.tween
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.barry.animation.R
+
+class TranslateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_translate)
+    }
+}
