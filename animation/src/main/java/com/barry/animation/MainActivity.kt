9f7e4da6b0cb642008eefe6e39e96f81fc081b4f
Barry <865068275@qq.com>
2020-10-26 15:21:53 +0800

    位移补间动画

--- animation/src/main/java/com/barry/animation/MainActivity.kt
+++ animation/src/main/java/com/barry/animation/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.barry.animation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.barry.animation.tween.TranslateActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn_translate).setOnClickListener {
+            startActivity(Intent(this, TranslateActivity().javaClass))
+        }
     }
 }
--- animation/src/main/java/com/barry/animation/tween/TranslateActivity.kt
+++ animation/src/main/java/com/barry/animation/tween/TranslateActivity.kt
@@ -2,11 +2,28 @@ package com.barry.animation.tween
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import android.widget.Button
+import android.widget.TextView
 import com.barry.animation.R
 
 class TranslateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translate)
+
+        findViewById<Button>(R.id.btn_xml).setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.translate);
+            findViewById<TextView>(R.id.tv_demo).startAnimation(animation);
+        }
+
+        findViewById<Button>(R.id.btn_code).setOnClickListener {
+            val animation = TranslateAnimation(0.0f, 400f, 0f, 200f)
+            animation.duration = 2000
+            animation.fillBefore = true
+
+            findViewById<TextView>(R.id.tv_demo).startAnimation(animation);
+        }
     }
 }
