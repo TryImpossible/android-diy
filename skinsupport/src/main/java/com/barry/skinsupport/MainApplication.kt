b313b12a56f464399ed889114b45212f2d6a17bf
Barry <865068275@qq.com>
2020-12-15 08:39:37 +0800

    创建skinsupport工程

---
+++ skinsupport/src/main/java/com/barry/skinsupport/MainApplication.kt
@@ -0,0 +1,23 @@
+package com.barry.skinsupport
+
+import android.app.Application
+import skin.support.SkinCompatManager
+import skin.support.app.SkinAppCompatViewInflater
+import skin.support.app.SkinCardViewInflater
+import skin.support.constraint.app.SkinConstraintViewInflater
+import skin.support.design.app.SkinMaterialViewInflater
+
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        SkinCompatManager.withoutActivity(this)
+                .addInflater(SkinAppCompatViewInflater()) // 基础控件换肤初始化
+                .addInflater(SkinMaterialViewInflater()) // material design 控件换肤初始化[可选]
+                .addInflater(SkinConstraintViewInflater()) // ConstraintLayout 控件换肤初始化[可选]
+                .addInflater(SkinCardViewInflater()) // CardView v7 控件换肤初始化[可选]
+                .setSkinStatusBarColorEnable(false) // 关闭状态栏换肤，默认打开[可选]
+                .setSkinWindowBackgroundEnable(false) // 关闭windowBackground换肤，默认打开[可选]
+                .loadSkin()
+    }
+}
