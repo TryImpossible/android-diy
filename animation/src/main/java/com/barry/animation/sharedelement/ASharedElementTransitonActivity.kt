a6b44977e63fec89ded19469bc2a0f244fcd6161
Barry <865068275@qq.com>
2020-12-29 19:29:02 +0800

    完善共享元素动画

--- animation/src/main/java/com/barry/animation/sharedelement/ASharedElementTransitonActivity.kt
+++ animation/src/main/java/com/barry/animation/sharedelement/ASharedElementTransitonActivity.kt
@@ -1,8 +1,10 @@
 package com.barry.animation.sharedelement
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.core.app.ActivityOptionsCompat
 import com.barry.animation.R
@@ -13,10 +15,31 @@ class ASharedElementTransitonActivity : AppCompatActivity() {
         setContentView(R.layout.activity_a_shared_element_transiton)
 
         val imageView = findViewById<ImageView>(R.id.iv_image)
-        imageView.setOnClickListener {
-            val bundle = ActivityOptionsCompat.makeSceneTransitionAnimation(this, imageView, "shareElement").toBundle()
+        findViewById<Button>(R.id.btn_enter1).setOnClickListener {
+            val activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this, imageView, "shareElement")
             val intent = Intent(this, BSharedElementTransitonActivity().javaClass)
-            startActivity(intent, bundle)
+            startActivity(intent, activityOptionsCompat.toBundle())
+        }
+        findViewById<Button>(R.id.btn_enter2).setOnClickListener {
+            val activityOptionsCompat = ActivityOptionsCompat.makeCustomAnimation(this, R.anim.translate_left_in, R.anim.translate_right_out)
+            val intent = Intent(this, BSharedElementTransitonActivity().javaClass)
+            startActivity(intent, activityOptionsCompat.toBundle())
+        }
+        findViewById<Button>(R.id.btn_enter3).setOnClickListener {
+            val activityOptionsCompat = ActivityOptionsCompat.makeClipRevealAnimation(imageView, 0, 0, 72, 72)
+            val intent = Intent(this, BSharedElementTransitonActivity().javaClass)
+            startActivity(intent, activityOptionsCompat.toBundle())
+        }
+        findViewById<Button>(R.id.btn_enter4).setOnClickListener {
+            val activityOptionsCompat = ActivityOptionsCompat.makeScaleUpAnimation(imageView, imageView.width / 2, imageView.height / 2, 0, 0)
+            val intent = Intent(this, BSharedElementTransitonActivity().javaClass)
+            startActivity(intent, activityOptionsCompat.toBundle())
+        }
+        findViewById<Button>(R.id.btn_enter5).setOnClickListener {
+            val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher_round)
+            val activityOptionsCompat = ActivityOptionsCompat.makeThumbnailScaleUpAnimation(imageView, bitmap, 72, 72)
+            val intent = Intent(this, BSharedElementTransitonActivity().javaClass)
+            startActivity(intent, activityOptionsCompat.toBundle())
         }
     }
 }
--- animation/src/main/java/com/barry/animation/sharedelement/BSharedElementTransitonActivity.kt
+++ animation/src/main/java/com/barry/animation/sharedelement/BSharedElementTransitonActivity.kt
@@ -2,11 +2,22 @@ package com.barry.animation.sharedelement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 import com.barry.animation.R
 
 class BSharedElementTransitonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b_shared_element_transiton)
+
+        findViewById<Button>(R.id.btn_exit).setOnClickListener {
+            ActivityCompat.finishAfterTransition(this)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        ActivityCompat.finishAfterTransition(this)
     }
 }
