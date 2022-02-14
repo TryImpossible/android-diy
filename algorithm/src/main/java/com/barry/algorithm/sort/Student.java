e2ada9b98d7e5b182aef48d634d22ae5aeec0591
Barry <865068275@qq.com>
2022-02-15 00:03:16 +0800

    创建algorithm工程

---
+++ algorithm/src/main/java/com/barry/algorithm/sort/Student.java
@@ -0,0 +1,35 @@
+package com.barry.algorithm.sort;
+
+public class Student implements Comparable<Student>{
+    private String username;
+    private int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.getAge() - o.getAge();
+    }
+}
---
+++ algorithm/src/test/java/com/barry/algorithm/sort/TestComparable.java
@@ -0,0 +1,29 @@
+package com.barry.algorithm.sort;
+
+public class TestComparable {
+    public static void main(String[] args) {
+        // 创建两个Student对象，并调用getMax方法，完成测试
+        Student s1 = new Student();
+        s1.setUsername("张三");
+        s1.setAge(18);
+
+        Student s2 = new Student();
+        s2.setUsername("李四");
+        s2.setAge(20);
+
+        Comparable max = getMax(s1, s2);
+        System.out.println(max);
+    }
+
+    public static Comparable getMax(Comparable c1, Comparable c2) {
+        int result = c1.compareTo(c2);
+        // 如果result>0，则c1比c2大；
+        // 如果result<0，则c1比c2小；
+        // 如果result=0，则c1和c2一样大；
+        if (result >= 0) {
+            return c1;
+        } else {
+            return c2;
+        }
+    }
+}
