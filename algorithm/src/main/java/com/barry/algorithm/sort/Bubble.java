1951f799d9d6caf091dfc25cb3cdba4884de8447
Barry <865068275@qq.com>
2022-02-15 13:08:09 +0800

    冒泡排序

---
+++ algorithm/src/main/java/com/barry/algorithm/sort/Bubble.java
@@ -0,0 +1,42 @@
+package com.barry.algorithm.sort;
+
+public class Bubble {
+    /**
+     * 对数组a的元素进行排序
+     *
+     * @param a
+     */
+    public static void sort(Comparable[] a) {
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (greater(a[j], a[j + 1])) {
+                    exch(a, j, j + 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * 比较v元素是否大于w元素
+     *
+     * @param v
+     * @param w
+     * @return
+     */
+    private static boolean greater(Comparable v, Comparable w) {
+        return v.compareTo(w) > 0;
+    }
+
+    /**
+     * 交换a数组中，索引i和索引j处的值
+     *
+     * @param a
+     * @param i
+     * @param j
+     */
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
---
+++ algorithm/src/test/java/com/barry/algorithm/sort/BubbleTest.java
@@ -0,0 +1,11 @@
+package com.barry.algorithm.sort;
+
+import java.util.Arrays;
+
+public class BubbleTest {
+    public static void main(String[] args) {
+        Integer[] arr = {4, 5, 6, 3, 2, 1};
+        Bubble.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
