58da6c70c95e3d1d21deb11b3e473bb8f9eaf43c
xiebin <865068275@qq.com>
2022-05-28 21:22:06 +0800

    最大优先队列

---
+++ algorithm/src/test/java/com/barry/algorithm/priority/MaxPriorityQueueTest.java
@@ -0,0 +1,22 @@
+package com.barry.algorithm.priority;
+
+public class MaxPriorityQueueTest {
+    public static void main(String[] args) {
+        // 创建优先队列
+        MaxPriorityQueue<String> queue = new MaxPriorityQueue<String>(10);
+        // 往队列中存储元素
+        queue.insert("A");
+        queue.insert("B");
+        queue.insert("C");
+        queue.insert("D");
+        queue.insert("E");
+        queue.insert("F");
+        queue.insert("G");
+
+        // 通过循环从队列中获取最大的元素
+        while (!queue.isEmpty()) {
+            String max = queue.delMax();
+            System.out.print(max + " ");
+        }
+    }
+}
