8920dc4e89601caf45df5d7d95409f599a963cc8
Barry <865068275@qq.com>
2024-12-07 13:30:14 +0800

    线程死锁示例

--- java/src/main/java/com/barry/java/Thread/DeadLock.java
+++ java/src/main/java/com/barry/java/Thread/DeadLock.java
@@ -8,6 +8,13 @@ public class DeadLock {
         Thread t2 = new Thread(new T1(obj1, obj2));
         t1.start();
         t2.start();
+
+//        DeadLock2 td1 = new DeadLock2();
+//        DeadLock2 td2 = new DeadLock2();
+//        td1.flag = 1;
+//        td2.flag = 0;
+//        new Thread(td1).start();
+//        new Thread(td2).start();
     }
 }
 
@@ -51,6 +58,40 @@ class T2 implements Runnable {
                 e.printStackTrace();
             }
             synchronized (obj1) {}
+        }
+    }
+}
+
+class DeadLockExample implements Runnable{
+    public int flag = 1;
+    private static Object o1 = new Object(), o2 = new Object();
+    @Override
+    public void run() {
+        System.out.println("flag=" + flag);
+        if (flag == 1){
+            synchronized (o1) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                synchronized (o2) {
+                    System.out.println("1");
+                }
+            }
+        }
+
+        if (flag == 0){
+            synchronized (o2) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                synchronized (o1) {
+                    System.out.println("0");
+                }
+            }
         }
     }
 }
